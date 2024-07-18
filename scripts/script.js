757cc84502ccbb225319c8af4fe489ba2b594a94
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-07-18 15:14:22 +0200

    add a error/chrash handler and render function

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,46 @@
-let API_KEY = "https://pokeapi.co/api/v2/"
+let API_KEY = "https://pokeapi.co/api/v2/";
+
+window.onload = init();
 
 function init() {
-    loadData();
-}
-
-async function read() {
-    try {
-        let response = await loadData();
-        if (response) {
-            let userKeysArray = Object.keys(response);
-            console.log(userKeysArray);
-        } else {
-            console.error("Response konnte nicht geladen werden (null oder undefined")
-        }
-    } catch (error) {
-        console.error("Error in read function:", error);
-    }
-}
-
-
-
-
-async function renderData() {
-
+    renderData();
 }
 
 async function loadData() {
     try {
         let response = await fetch(API_KEY + "pokemon?limit=30&offset=0");
         let responseToJson = await response.json();
-        console.log(responseToJson);
         return responseToJson;
     } catch (error) {
         console.error("Error in loadData function:", error);
         return null;
     }
 }
+
+async function loadPokemonDetails(url) {
+    try {
+        let response = await fetch(url);
+        let responseToJson = await response.json();
+        return responseToJson;
+    } catch (error) {
+        console.error("Error in loadPokemonDetails function:", error);
+        return null;
+    }
+}
+
+async function renderData() {
+    let data = await loadData();
+    let content = document.getElementById("render");
+    content.innerHTML = ``;
+    if (data && data.results) {
+        for (let i = 0; i < data.results.length; i++) {
+            let pokemon = data.results[i];
+            let pokemonDetails = await loadPokemonDetails(pokemon.url);
+            content.innerHTML += renderInDiv(pokemon, pokemonDetails);
+        }
+    } else {
+        content.innerHTML = "No data available";
+    }
+}
+
+
--- templates/poke-render.js
+++ templates/poke-render.js
@@ -1,11 +1,13 @@
-function renderInDiv() {
+function renderInDiv(pokemon, pokemonDetails) {
     return `
         <div class="col">
             <img src="./25.png" class="img-fluid border rounded card-img-top" alt="IMG" />
             <div class="card-body">
                 <p class="card-text">
-                    Some quick example text to build on the card title and make up
-                    the bulk of the card's content.
+                    Name: <b>${pokemon.name}</b> <br> 
+                    API Link: <b>${pokemon.url}</b>
+                    ID: <b>${pokemonDetails.id}</b>
+
                 </p>
             </div>
         </div>
