6ad0fdbebbf097a4667cef65bcaa7bc3021b366c
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-07-18 16:27:45 +0200

    add image from pokemons and edit style

--- templates/poke-render.js
+++ templates/poke-render.js
@@ -1,13 +1,13 @@
 function renderInDiv(pokemon, pokemonDetails) {
     return `
         <div class="col">
-            <img src="./25.png" class="img-fluid border rounded card-img-top" alt="IMG" />
+            <img src="${pokemonDetails.sprites.front_default}" class="poke-img img-fluid border rounded card-img-top" alt="${pokemonDetails.sprites.front_default}" />
             <div class="card-body">
                 <p class="card-text">
                     Name: <b>${pokemon.name}</b> <br> 
-                    API Link: <b>${pokemon.url}</b>
-                    ID: <b>${pokemonDetails.id}</b>
-
+                    API Link: <b>${pokemon.url}</b> <br>
+                    ID: <b>${pokemonDetails.id}</b> <br>
+                    IMG_URL: ${pokemonDetails.sprites.front_default}
                 </p>
             </div>
         </div>
