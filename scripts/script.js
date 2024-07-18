95c80a4700991956e0bf4485546984e1da6b2403
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-07-18 14:11:51 +0200

    add console log testing API

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,38 @@
 let API_KEY = "https://pokeapi.co/api/v2/"
 
-async function loadData() {
-    let response = await fetch(API_KEY + "pokemon?limit=5&offset=0");
-    let responseToJson = await response.json();
-    console.log(responseToJson);
+function init() {
+    loadData();
+}
+
+async function read() {
+    try {
+        let response = await loadData();
+        if (response) {
+            let userKeysArray = Object.keys(response);
+            console.log(userKeysArray);
+        } else {
+            console.error("Response konnte nicht geladen werden (null oder undefined")
+        }
+    } catch (error) {
+        console.error("Error in read function:", error);
+    }
 }
 
 
+
+
 async function renderData() {
-    
+
+}
+
+async function loadData() {
+    try {
+        let response = await fetch(API_KEY + "pokemon?limit=30&offset=0");
+        let responseToJson = await response.json();
+        console.log(responseToJson);
+        return responseToJson;
+    } catch (error) {
+        console.error("Error in loadData function:", error);
+        return null;
+    }
 }
---
+++ templates/poke-render.js
@@ -0,0 +1,13 @@
+function renderInDiv() {
+    return `
+        <div class="col">
+            <img src="./25.png" class="img-fluid border rounded card-img-top" alt="IMG" />
+            <div class="card-body">
+                <p class="card-text">
+                    Some quick example text to build on the card title and make up
+                    the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+    `
+}
