0f7346c69ff44a36ce505281c5e5778155b8620a
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-07-20 15:15:32 +0200

    Add Types of Pokemons
    edit Styleing of cards

--- templates/poke-render.js
+++ templates/poke-render.js
@@ -1,13 +1,17 @@
 function renderInDiv(i, pokemon, pokemonDetails) {
+    let typesHtml = `<span class="padding-3 rounded border border-dark">${pokemonDetails.types[0].type.name}</span>`;
+    if (pokemonDetails.types[1]) {
+        typesHtml += ` <span class="padding-3 rounded border border-dark">${pokemonDetails.types[1].type.name}</span>`;
+    }
+    
     return `
         <div onclick="viewPokemon(${i})" class="hover-effect cursor-pointer margin-10 shadow p-3 card" style="width: 18rem;">
         <span class="fw-lighter">PokeID:${pokemonDetails.id}</span>
         <img src="${pokemonDetails.sprites.front_default}" class="card-img-top" alt="${pokemonDetails.sprites.front_default}">
             <div class="card-body">
                 <p class="card-text fw-bold">${pokemon.name}</p>
-                <span>Dummy</span> <br>
+                ${typesHtml} <br><br>
                 <span class="fw-lighter">PokeID:${pokemonDetails.id}</span>
-
             </div>
         </div>
     `
