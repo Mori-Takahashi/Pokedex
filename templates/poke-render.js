6240e9647725d12d4ef98f2367b8a064aef70e56
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-07-20 14:17:37 +0200

    Update poke-render.js
    remove html

--- templates/poke-render.js
+++ templates/poke-render.js
@@ -12,16 +12,3 @@ function renderInDiv(pokemon, pokemonDetails) {
         </div>
     `
 }
-
-/*         <div class="col">
-            <img src="${pokemonDetails.sprites.front_default}" class="poke-img img-fluid border rounded card-img-top" alt="${pokemonDetails.sprites.front_default}" />
-            <div class="card-body">
-                <p class="card-text">
-                    Name: <b>${pokemon.name}</b> <br> 
-                    API Link: <b>${pokemon.url}</b> <br>
-                    ID: <b>${pokemonDetails.id}</b> <br>
-                    IMG_URL: ${pokemonDetails.sprites.front_default}
-                </p>
-            </div>
-        </div>
- */
