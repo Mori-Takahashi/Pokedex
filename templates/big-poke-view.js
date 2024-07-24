8901d421a7de434549964fee7378d0875ab19cfe
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-07-24 12:20:49 +0200

    all comments in the code removed

--- templates/big-poke-view.js
+++ templates/big-poke-view.js
@@ -10,7 +10,7 @@ function renderPokeInfosBigView(data, pokemonColor, pokemonColor1) {
             <button type="button" class="btn-close" onclick="closeWindow()" aria-label="Close"></button>
           </div>
         </nav>
-        <div class="d-flex justify-content-around"><!-- container for IMG -->
+        <div class="d-flex justify-content-around">
           <img src="${data.sprites.front_default}" alt="${data.sprites.front_default}">
           <div class="infoText">
             <span>National №: ${data.id}</span>
@@ -19,10 +19,7 @@ function renderPokeInfosBigView(data, pokemonColor, pokemonColor1) {
             <span>Weight: ${data.weight}</span>
             <span>Abilities: ${data.abilities[0].ability.name}</span>
             <span>Local №: ${data.game_indices[0].game_index} // Game: ${data.game_indices[0].version.name}</span>
-            <!-- info of pokemon -->
           </div>
         </div>
-        <!-- render the infos -->
-        <!-- class scale-out-vertical AND scale-up-ver-center -->
     `
 }
