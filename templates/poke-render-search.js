cc4e64e20a3b034b1c34b17b27098376a26ee88c
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-07-26 09:04:38 +0200

    wrong information when searching

--- templates/poke-render-search.js
+++ templates/poke-render-search.js
@@ -6,12 +6,12 @@ function renderInDivSearch(data, pokemonColor) {
     
     return `
         <div style="${pokemonColor}" onclick="viewPokemon(${data.id})" class="hover-effect cursor-pointer margin-10 shadow p-3 card" style="width: 18rem;">
-        <span class="fw-lighter">PokeID:${data.id}</span>
+        <span class="fw-lighter">National №: ${data.id}</span>
         <img src="${data.sprites.front_default}" class="card-img-top" alt="${data.sprites.front_default}">
             <div class="card-body">
                 <p class="card-text fw-bold">${data.forms[0].name}</p> 
                 ${typesHtml} <br><br>
-                <span class="fw-lighter">PokeID:${data.id}</span>
+                <span class="fw-lighter">Local №: ${data.game_indices[0].game_index} in ${data.game_indices[0].version.name}</span>
             </div>
         </div>
     `
